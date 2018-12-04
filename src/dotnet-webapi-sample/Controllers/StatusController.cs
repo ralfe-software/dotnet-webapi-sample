ed1b56a28c5d0148ed90481a2aceebbfa1470f54
Ralf Ehlert <r.ehlert@cid.com>
2018-12-04 11:18:42 +0100

    FEAT initial commit with project setup

---
+++ src/dotnet-webapi-sample/Controllers/StatusController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetWebApiSample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        public ActionResult<OkResult> Get()
+        {
+            return Ok();
+        }
+    }
+}
